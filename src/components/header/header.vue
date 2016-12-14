a35165638c62f2ef7977b56e22af9bea1fe6ce9c
Jonathan <jonrcao@gmail.com>
2016-12-15 01:16:32 +0800

    add proj

---
+++ src/components/header/header.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="header">
+        <div class="content-wrapper">
+            <div class="avatar">
+                <img :src="seller.avatar" width="64" height="64" />
+            </div>
+            <div class="content">
+                <div class="title">
+                    <span class="barnd"></span>
+                    <span class="name">{{seller.name}}</span>
+                </div>
+                <div class="description">
+                    {{seller.description}}/{{seller.deliveryTime}}分钟送达
+                </div>
+                <div v-if="seller.supports" class="supports">
+                    <span class="icon"></span>
+                    <span class="text">{{seller.supports[0].description}}</span>
+                </div>
+            </div>
+        </div>
+        <div class="bulletin-wrapper"></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            seller: {
+                type: Object
+            }
+        }
+    };
+</script>
+
+<style lang="scss" rel="stylesheet/scss">
+    .header {
+        color: #fff;
+        background-color: #000;
+        .content-wrapper {
+            padding: 24px 12px 18px 24px;
+            > div {
+                display: inline-block;
+            }
+        }
+    }
+</style>
