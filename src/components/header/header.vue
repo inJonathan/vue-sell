bda247757ac2572a3bc802a273a9f7a8d87514bc
Jonathan <jonrcao@gmail.com>
2016-12-16 18:17:21 +0800

    浮层创建

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -17,17 +17,34 @@
                     <span class="text">{{seller.supports[0].description}}</span>
                 </div>
             </div>
-            <div v-if="seller.supports" class="support-count">
+            <div v-if="seller.supports" class="support-count" @click="showDetail">
                 <span class="count">{{seller.supports.length}}个</span>
                 <i class="icon-keyboard_arrow_right"></i>
             </div>
         </div>
-        <div class="bulletin-wrapper">
+        <div class="bulletin-wrapper" @click="showDetail">
             <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
             <i class="icon-keyboard_arrow_right"></i>
         </div>
         <div class="background">
             <img :src="seller.avatar" width="100%" height="100%" />
+        </div>
+        <div v-show="detailShow" class="detail">
+            <div class="detail-wrapper clearfix">
+                <div class="detail-main">
+                    <p>{{seller.bulletin}}</p>
+                    <p>{{seller.bulletin}}</p>
+                    <p>{{seller.bulletin}}</p>
+                    <p>{{seller.bulletin}}</p>
+                    <p>{{seller.bulletin}}</p>
+                    <p>{{seller.bulletin}}</p>
+                    <p>{{seller.bulletin}}</p>
+                    <p>{{seller.bulletin}}</p>
+                </div>
+            </div>
+            <div class="detail-close">
+                <i class="icon-close"></i>
+            </div>
         </div>
     </div>
 </template>
@@ -37,6 +54,16 @@
         props: {
             seller: {
                 type: Object
+            }
+        },
+        data() {
+            return {
+                detailShow: false
+            };
+        },
+        methods: {
+            showDetail() {
+                this.detailShow = true;
             }
         },
         created() {
@@ -52,6 +79,7 @@
     .header {
         position: relative;
         color: #fff;
+        overflow: hidden;
         background-color: rgba(7,17,27,.5);
         .content-wrapper {
             position: relative;
@@ -185,6 +213,31 @@
             height: 100%;
             z-index: -1;
             filter: blur(10px);
+        }
+        .detail {
+            position: fixed;
+            z-index: 100;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            overflow: auto;
+            background: rgba(7,17,27,.8);
+            .detail-wrapper {
+                min-height: 100%;
+                .detail-main {
+                    margin-top: 64px;
+                    padding-bottom: 64px;
+                }
+            }
+            .detail-close {
+                position: relative;
+                width: 32px;
+                height: 32px;
+                margin: -64px auto 0 auto;
+                clear: both;
+                font-size: 32px;
+            }
         }
     }
 </style>
