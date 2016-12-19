30542e537b3d33e8478397c7c371629eb65fc97d
Jonathan <jonrcao@gmail.com>
2016-12-19 17:19:16 +0800

    菜单列表滚动和联动完成

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,17 +1,17 @@
 <template>
     <div class="goods">
-        <div class="menu-wrapper">
+        <div class="menu-wrapper" v-el:menu-wrapper>
             <ul>
-                <li v-for="item in goods" class="menu-item">
+                <li v-for="item in goods" class="menu-item" :class="{'current': currentIndex === $index}" @click="selectMenu($index, $event)">
                     <span class="text border-1px">
             	        <span v-show="item.type > 0" class="icon" :class="classMap[item.type]"></span> {{item.name}}
                     </span>
                 </li>
             </ul>
         </div>
-        <div class="foods-wrapper">
+        <div class="foods-wrapper" v-el:foods-wrapper>
             <ul>
-                <li v-for="item in goods" class="food-list">
+                <li v-for="item in goods" class="food-list food-list-hook">
                     <h1 class="title">{{item.name}}</h1>
                     <ul>
                         <li v-for="food in item.foods" class="food-item border-1px">
@@ -22,12 +22,10 @@
                                 <h2 class="name">{{food.name}}</h2>
                                 <p class="desc">{{food.description}}</p>
                                 <div class="extra">
-                                    <span class="count">月售{{food.sellCount}}份</span>
-                                    <span>好评率{{food.rating}}%</span>
+                                    <span class="count">月售{{food.sellCount}}份</span><span>好评率{{food.rating}}%</span>
                                 </div>
                                 <div class="price">
-                                    <span class="now">&yen; {{food.price}}</span>
-                                    <span class="old" v-show="food.oldPrice">&yen; {{food.oldPrice}}</span>
+                                    <span class="now">&yen; {{food.price}}</span><span class="old" v-show="food.oldPrice">&yen; {{food.oldPrice}}</span>
                                 </div>
                             </div>
                         </li>
@@ -39,6 +37,8 @@
 </template>
 
 <script>
+    import BScroll from 'better-scroll';
+
     const ERR_OK = 0;
 
     export default {
@@ -49,8 +49,23 @@
         },
         data() {
             return {
-                goods: []
+                goods: [],
+                listHeight: [],
+                scrollY: 0
             };
+        },
+        computed: {
+            currentIndex() {
+                for (let i = 0; i < this.listHeight.length; i++) {
+                    // 拿到listHeight里面的当前值和后一个值，如果当前屏幕滚动值落在这个区间，则映射到侧边菜单栏
+                    let height1 = this.listHeight[i];
+                    let height2 = this.listHeight[i + 1];
+                    if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+                        return i;
+                    }
+                }
+                return 0;
+            }
         },
         created() {
             this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
@@ -58,8 +73,45 @@
                 response = response.body;
                 if(response.errno === ERR_OK) {
                     this.goods = response.data;
+                    // vue默认是异步执行方法，这里要获得元素的高度，所以要使用这个方法让dom更新后才执行回调
+                    this.$nextTick(() => {
+                        this._initScroll();
+                        this._calulateHeight();
+                    });
                 }
             });
+        },
+        methods: {
+            selectMenu(index, event) {
+                if (!event._constructed) { // 遇到自定义派发事件的时候event._constructed为true
+                    return;
+                }
+                let foodList = this.$els.foodsWrapper.getElementsByClassName('food-list-hook');
+                let el = foodList[index];
+                this.foodsScroll.scrollToElement(el, 300);
+            },
+            _initScroll() {
+                this.menuScroll = new BScroll(this.$els.menuWrapper, {
+                    click: true
+                });
+                this.foodsScroll = new BScroll(this.$els.foodsWrapper, {
+                    probeType: 3
+                });
+                this.foodsScroll.on('scroll', (pos) => {
+                    this.scrollY = Math.abs(Math.round(pos.y));
+                });
+            },
+            _calulateHeight() {
+                let foodList = this.$els.foodsWrapper.getElementsByClassName('food-list-hook');
+                let height = 0;
+                this.listHeight.push(height);
+                // 遍历出每一个区间的高度，然后push到一个数组里面
+                for (let i = 0; i < foodList.length; i++) {
+                    let item = foodList[i];
+                    height += item.clientHeight;
+                    this.listHeight.push(height);
+                }
+            }
         }
     };
 </script>
@@ -83,6 +135,18 @@
                 height: 54px;
                 padding: 0 12px;
                 line-height: 14px;
+                &.current {
+                    position: relative;
+                    z-index: 10;
+                    margin-top: -1px;
+                    background: #fff;
+                    .text {
+                        font-weight: 700;
+                        &:after {
+                            display: none;
+                        }
+                    }
+                }
                 .icon {
                     display: inline-block;
                     vertical-align: middle;
@@ -153,7 +217,7 @@
                     }
                     .desc {
                         margin-bottom: 8px;
-                        line-height: 10px;
+                        line-height: 12px;
                         font-size: 10px;
                         color: rgb(147, 153, 159);
                     }
@@ -161,7 +225,7 @@
                         line-height: 10px;
                         font-size: 10px;
                         color: rgb(147, 153, 159);
-                        &.count {
+                        .count {
                             margin-right: 12px;
                         }
                     }
